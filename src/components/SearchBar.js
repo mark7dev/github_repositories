4a764693bdd857e318545fdb791939fe35fd963b
MarKoRn <mark7dev@hotmail.com>
2020-09-20 17:13:16 -0500

    handleChange for searchInput

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+
+    const [search, setSearch] = useState('');
+
+    const handleChange = e => {
+        setSearch(e.target.value)
+    }
+
     return ( 
         <div className="search__container">
             <input
+                name="searchInput"
                 className="searchInput" 
                 type="text"
-                placeholder="Search an issue"
+                placeholder="User"
+                onChange={handleChange}
             />
-            <button>
+            <button
+                disabled={search === ''}
+            >
                 <i className="fa fa-search icons" aria-hidden="true"></i>
             </button>
         </div>
