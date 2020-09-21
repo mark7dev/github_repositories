6c0c6ee375af621a0a155cad896a17eab4cd9619
MarKoRn <mark7dev@hotmail.com>
2020-09-20 19:29:52 -0500

    add spinner

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import Repos from './components/Repos';
+import Spinner from './components/Spinner';
 import './App.css';
 
 function App() {
 
   const [repositories, setRepositories] = useState([]);
   const [user, setUser] = useState('');
+  const [userData, setUserData] = useState();
+  const [loading, setLoading] = useState(false);
 
   const getRepos = user => {
-    axios.get(`https://api.github.com/users/${user}/repos?per_page=1000`)
+    setLoading(true);
+    setStates();
+    axios.get(`https://api.github.com/users/${user}`)
       .then(response => {
         setUser(user);
-        setRepositories(response.data)
+        setUserData(response.data);
+        axios.get(`https://api.github.com/users/${user}/repos?per_page=1000`)
+          .then(response => {
+            setLoading(false);
+            setRepositories(response.data)
+          })
+          .catch(error => {
+            setLoading(false);
+            console.log(error);
+          })
       })
       .catch(error => {
+        setLoading(false);
         console.log(error);
+        setStates();
       })
+  }
+
+  const setStates = () => {
+    setRepositories([]);
+    setUser('');
+    setUserData({});
   }
 
   return (
     <div className="App">
       <div className="container">
-          {user ?
-            <h1>{repositories.length} public repositories of {user}</h1>
-            :
-            <h1>Github repositories</h1>
-          }
+          <h1>Github repositories</h1>
           <SearchBar 
             getRepos={getRepos}
           />
+          {loading ? <Spinner /> : null}
+          { !loading && userData ?
+            <div>
+              <h3>{userData.login}</h3>
+              <h3>{userData.public_repos}</h3>
+            </div>
+            : null
+          }
           <Repos 
             repositories={repositories}
           />
